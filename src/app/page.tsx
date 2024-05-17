c15010b8c78e2754c432b80067ceb0f5da593d0f
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-05-17 14:17:18 -0300

    Moved global components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import { Submenu } from "./components/home/submenu";
+import { getHomeData } from "@/utils/actions/get-data";
+import { Submenu } from "../components/home/submenu";
+import { HomeProps } from "@/utils/home.type";
+import { Hero } from "@/components/hero";
+import { Phone } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { object: home }: HomeProps = await getHomeData();
+
+  console.log(home);
+
   return (
     <main>
       <Submenu />
+
+      <Hero
+        heading={home.metadata.heading}
+        buttonTitle={home.metadata.cta_button.title}
+        buttonUrl={home.metadata.cta_button.url}
+        bannerUrl={home.metadata.banner.url}
+        icon={<Phone size={24} color={"#fff"} />}
+      />
     </main>
   );
 }
